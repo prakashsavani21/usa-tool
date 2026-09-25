0490c0da6d446fca7b0111634670cf327a4779cb
prakashsavani21 <prakashsavani708@gmail.com>
2026-09-25 12:54:13 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,3 +4,6 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+// USA Free Tools 2.0 tool routes
+const USFT_NEW_TOOLS = ['/budget-planner.html', '/password-generator.html', '/word-counter.html', '/random-number.html', '/qr-generator.html', '/random-name-picker.html', '/username-generator.html', '/color-picker.html', '/text-case-converter.html', '/text-cleaner.html', '/json-formatter.html', '/url-encoder.html', '/uuid-generator.html', '/salary-calculator.html', '/split-bill.html', '/debt-payoff.html', '/savings-goal.html', '/countdown.html', '/date-difference.html', '/dice-roller.html'];
